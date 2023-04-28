bb56ffbbccb35ce65b69ee21ed970b9ec782e0dd
achingbrain <alex@achingbrain.net>
2023-04-28 14:18:45 +0100

    chore: fix windows

--- src/node/execa.ts
+++ src/node/execa.ts
@@ -1,27 +1,8 @@
 
-import fs from 'node:fs'
 import { execa, type ExecaChildProcess, type Options } from 'execa'
-import which from 'which'
-
-function isExecutable (command: string): boolean {
-  try {
-    fs.accessSync(command, fs.constants.X_OK)
-    return true
-  } catch (err: any) {
-    if (err.code === 'ENOENT') {
-      return isExecutable(which.sync(command))
-    }
-
-    if (err.code === 'EACCES') {
-      return false
-    }
-
-    throw err
-  }
-}
 
 function execaUtil (command: string, args: string[] = [], opts: Options<string> = {}, callback?: (proc: ExecaChildProcess<string>) => void): ExecaChildProcess<string> {
-  if (!isExecutable(command)) {
+  if (command.endsWith('.js')) {
     args.unshift(command)
     command = 'node'
   }
