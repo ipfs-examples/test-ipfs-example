1f84141a4c6c1df9e856f7e0ec211dd9396111ea
achingbrain <alex@achingbrain.net>
2023-04-28 12:50:15 +0100

    chore: increase ci timeout

--- test/browser.spec.ts
+++ test/browser.spec.ts
@@ -1,6 +1,9 @@
 import { execa } from 'execa'
 
-describe('browser', () => {
+describe('browser', function () {
+  // slow CI is slow
+  this.timeout(540000)
+
   it('should have browser success', async () => {
     await execa('./bin/test-browser-example.js', [
       './test/fixtures/browser/test.spec.js'
