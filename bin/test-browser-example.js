67b4dbdea07496947939606b40efef76ab63d54b
Alex Potsides <alex@achingbrain.net>
2024-10-08 16:12:05 +0100

    fix: tolerate failure to install browsers (#30)
    This is done elsewhere in monorepo test jobs and can cause problems when run in parallel

--- bin/test-browser-example.js
+++ bin/test-browser-example.js
@@ -3,10 +3,16 @@
 
 import { execa } from 'execa'
 
-// install deps
-await execa('npx', ['-y', 'playwright', 'install', '--with-deps'], {
-  stdio: 'inherit'
-})
+try {
+  // install deps
+  await execa('npx', ['-y', 'playwright', 'install', '--with-deps'], {
+    stdio: 'inherit'
+  })
+} catch (err) {
+  if (!err.message.includes('Could not get lock')) {
+    throw err
+  }
+}
 
 for (const file of process.argv.slice(2)) {
   // run test
