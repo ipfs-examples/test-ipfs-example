a4af46026c03003eb53740ace0ae3374827aea13
Alex Potsides <alex@achingbrain.net>
2024-10-08 16:07:19 +0100

    chore: apply suggestions from code review

--- bin/test-browser-example.js
+++ bin/test-browser-example.js
@@ -3,6 +3,16 @@
 
 import { execa } from 'execa'
 
+try {
+  // install deps
+  await execa('npx', ['-y', 'playwright', 'install', '--with-deps'], {
+    stdio: 'inherit'
+  })
+} catch (err) {
+  if (!err.message.includes('Could not get lock')) {
+    throw err
+  }
+}
 for (const file of process.argv.slice(2)) {
   // run test
   await execa('npx', ['playwright', 'test', file], {
